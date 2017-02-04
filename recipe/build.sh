d21ab928d061cd63ebe1fda7e58b4d4e4cc85dbd
Peter Williams <peter@newton.cx>
2017-02-04 13:54:34 -0500

    Tidy shared-library cleanup.

--- recipe/build.sh
+++ recipe/build.sh
@@ -49,10 +49,9 @@ make check
 
 rm -rf $uprefix/share/man
 
-# Prefer dynamic libraries to static, and dump libtool helper files
-for lib_ident in Xfixes; do
-    #rm -f $uprefix/lib/lib${lib_ident}.la
-    if [ -e $uprefix/lib/lib${lib_ident}$SHLIB_EXT ] ; then
-        rm -f $uprefix/lib/lib${lib_ident}.a
-    fi
-done
+# Non-Windows: prefer dynamic libraries to static, and dump libtool helper files
+if [ -z "VS_MAJOR" ] ; then
+    for lib_ident in Xfixes; do
+        rm -f $uprefix/lib/lib${lib_ident}.la $uprefix/lib/lib${lib_ident}.a
+    done
+fi
